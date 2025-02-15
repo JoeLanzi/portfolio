05f69f904e3015127a515c42143d14226f3b5be8
joelanzi@ymail.com <JoeLanzi@ymail.com>
2025-02-15 14:46:45 -0600

    finalized user about

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -101,9 +101,12 @@ const about = {
         timeframe: "Jan 2025 - Present",
         role: "AI Engineer",
         achievements: [
-          <>As one of the technical architects behind GuildIQ, I helped revolutionize knowledge access across Guild Mortgage by implementing a multi-agent retrieval-augmented 
-          generation (RAG) framework that fundamentally changed how teams interact with critical information. Through full-stack optimization strategies of the RAG pipeline, 
-          I significantly improved document processing speed, response time, and reduced operational overhead through efficient resource utilization.</>
+          <>As one of the key technical architects behind GuildIQ, I helped revolutionized knowledge access by developing 
+          a multi-agent retrieval-augmented generation (RAG) framework that transformed how teams interact with 
+          critical information. I implemented comprehensive full-stack optimization strategies across the RAG pipeline, 
+          significantly boosting document processing speeds, reducing response times, and cutting operational overhead 
+          through efficient resource management. This innovative approach reshaped internal workflows and set a new 
+          benchmark for leveraging AI in operational environments.</>
         ],
         images: [],
       },
@@ -112,11 +115,12 @@ const about = {
         timeframe: "Jan 2024 - Dec 2024",
         role: "Lead AI Engineer",
         achievements: [
-          <>I led the developedment of the core adaptive multi-modal system that set new standards for real-time environmental awareness in high-stakes operations. 
-          I drove the designing and deployment of optimized AI models across cloud and edge environments enabling unprecedented accuracy in threat detection and classification. 
-          The implementation of fine-tuned LLMs for specialized applications, coupled with development of high-performance auditory transcription systems, 
-          delivered a 10x improvement in processing speed compared to existing solutions at the time. This end-to-end AI pipeline architecture created a seamless bridge between 
-          cloud capabilities and edge platforms, establishing new benchmarks for AI performance in real-time mission-critical scenarios.</>
+          <>I led the design and deployment of an adaptive multi-modal system that redefined real-time environmental 
+          awareness in high-stakes operations. By orchestrating the development of state-of-the-art AI models across 
+          cloud and edge platforms, I achieved exceptional accuracy in threat detection and classification. 
+          Integrating fine-tuned large language models with high-performance auditory transcription systems resulted 
+          in a tenfold increase in processing speeds, establishing a robust, mission-critical AI pipeline that 
+          seamlessly bridges cloud capabilities with on-ground operations.</>
         ],
         images: [],
       },
@@ -125,11 +129,12 @@ const about = {
         timeframe: "Aug 2022 - Dec 2023",
         role: "AI Data Scientist",
         achievements: [
-          <>I wore multiple hats at OCI as a data scientist consultant like serving as lead architect for advanced geospatial ML solutions where I established new standards 
-          in environmental analysis through collaboration with Google and Planet Data Labs. I also developed of sophisticated AI systems for crop prediction and natural disaster 
-          analysis accelerating response times while reducing manual intervention. I was also responsible for deployment of production-grade ML systems such as malware 
-          categorization and implementation of fraud detection models for the healthcare industry.
-          These initiatives showcased the practical application of AI in solving complex operational challenges.</>
+          <>I played a pivotal role as a data scientist and lead architect for advanced geospatial analytics 
+          solutions collaborating with industry leaders like Google and Planet Data Labs, where I developed innovative 
+          methods for environmental analysis that drove breakthroughs in crop prediction and natural disaster 
+          assessment. Additionally, I oversaw the development and deployment of production-grade models for malware 
+          categorization and fraud detection in healthcare, demonstrating the significant impact of data-driven 
+          solutions on complex, real-world challenges.</>
         ],
         images: [],
       },
@@ -138,16 +143,20 @@ const about = {
         timeframe: "Aug 2021 - Dec 2022",
         role: "Research Assistant / Deep Learning Programmer",
         achievements: [
-          <>Through my role in advancing visual categorization systems, I directly enhanced the capabilities of human trafficking investigations at the National Center for Missing and Exploited Children. My parallel development of a sophisticated reinforcement learning model for virtual economic simulation demonstrated the versatility of AI applications in both practical and theoretical domains. The success of these initiatives showcased how targeted AI solutions can simultaneously address critical social issues while advancing our understanding of complex systemic behaviors.</>
+          <>I enhanced deep learning techniques for image search systems supporting critical human trafficking 
+          investigations, and developed a reinforcement learning model that simulated virtual economies—delivering 
+          fresh insights into adaptive behaviors that drive real-world impact.</>
         ],
         images: [],
       },
       {
-        company: "The New York Stem Cell Foundation Research Institute",
+        company: "NYSCF",
         timeframe: "Jun 2020 - May 2021",
         role: "Machine Learning Engineer",
         achievements: [
-          <>By developing a sophisticated CNN architecture for cell segmentation, I enabled unprecedented accuracy in analyzing both nuclear and non-nuclear stains. My implementation of deep learning object detection algorithms created new possibilities for identifying specific biological characteristics in nuclei, while my innovative approach to 3D tumor cell classification and growth forecasting provided researchers with essential tools for understanding cancer progression. These developments directly contributed to advancing the foundation's research capabilities in stem cell analysis and cancer research.</>
+          <>I merged deep learning with medical research by crafting a custom CNN for precise cell segmentation and 
+          designing object detection methods to capture key biological markers. My work in 3D tumor cell classification 
+          sharpened our ability to predict cancer progression with greater accuracy.</>
         ],
         images: [],
       },
@@ -156,7 +165,9 @@ const about = {
         timeframe: "Jan 2019 - May 2020",
         role: "Research Assistant / Deep Learning Programmer",
         achievements: [
-          <>Through my work with neuromorphic vision sensors, I pioneered bio-inspired vision systems that achieved breakthrough performance in classification and tracking. My development of deep learning techniques for motion-based target discrimination, combined with efficient scene analysis strategies, established new standards for computer vision under resource constraints. The culmination of this work - a partial facial recognition system achieving 99% accuracy for individuals with facial coverings - demonstrated the practical impact of my research in addressing real-world security challenges.</>
+          <>I utilized advanced neuromorphic vision sensors to create bio-inspired vision systems that excel in challenging 
+          conditions. By improving contrast sensitivity and developing deep learning strategies for rapid scene analysis, 
+          I built a facial recognition solution that achieved 99% accuracy even under conditions with facial coverings.</>
         ],
         images: [],
       },
@@ -165,10 +176,10 @@ const about = {
         timeframe: "Jun 2013 - Jun 2018",
         role: "Intelligence Support / Instructor",
         achievements: [
-          <>As a key instructor and intelligence support specialist, I elevated the Marine Corps' operational capabilities through comprehensive 
-          training programs reaching over 20,000 personnel. My development of organizational-level lesson plans and delivery of more than 400 hours of 
-          specialized instruction strengthened the Corps' framework. There, my main responsibility was to analyze real time operational elements 
-          and provided strategic briefs to support future operations.</>
+          <>As an instructor, I crafted targeted lesson plans and delivered over 400 hours of specialized training 
+          to more than 20,000 personnel, significantly enhancing operational readiness. Separately, in my role as an 
+          intelligence support specialist, I analyzed real-time operational elements and provided strategic briefs to 
+          shape future decisions.</>
         ],
         images: [],
       },
